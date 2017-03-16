db9573032bcb4cb8b74a62ea454df41aa74d9720
Andy Dojon <andrewldonjon@hotmail.com>
2017-03-16 15:33:56 -0500

    Final changes and CSS styling

---
+++ pyramid.js
@@ -0,0 +1,29 @@
+function drawPyramid(height) {
+
+    var pyramidShape = document.getElementById("shape").value;
+    document.getElementById("pyramid").innerHTML="" + pyramidShape;
+
+    var pyramidHeight = document.getElementById("height").value;
+    document.getElementById("pyramid").innerHTML = "" + pyramidHeight;
+
+    document.getElementById("pyramid").innerHTML = "";
+
+    for (var row = 0; row < height; row++) {
+
+        var numBricks = row + 2;
+        var numSpaces = height - row - 1;
+
+        var rowStr = "";
+        for (var i = 0; i < numSpaces; i++) {
+            var spaceChar = "&nbsp";
+            rowStr += spaceChar;
+        }
+        for (var i = 0; i < numBricks; i++) {
+            rowStr += pyramidShape;
+        }
+        rowElem = document.createElement("p");
+        rowElem.innerHTML = rowStr;
+        document.getElementById("pyramid").appendChild(rowElem);
+
+    }
+}
